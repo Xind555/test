3228748e354ca82fbf58a736e3c64a86892cb36a
Xind555 <wax.guibert@laposte.net>
2016-05-26 16:03:02 +0200

    python push 9

--- tests/code.py
+++ tests/code.py
@@ -1,15 +1,23 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+"""Math class"""
+
+@classmethod
 class Math():
-
-	def sum(self, a, b):  
-    		return a + b
-
-	def subs(self, a, b):
-			return a - b
 	
-	def mul(self, a, b):
-			return a * b
+	@staticmethod
+	def sum(var1, var2):
+		"""sum"""
+		return var1 + var2
 
+	@staticmethod
+	def subs(var1, var2):
+		"""subs"""
+		return var1 - var2
 
+	@staticmethod
+	def mul(var1, var2):
+		"""mul"""
+		return var1 * var2
+	
--- tests/tests_X_Y_20150206.py
+++ tests/tests_X_Y_20150206.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+"""bla"""
+
 #nosetests -x --verbosity=3 --with-xunit --xunit-file=nosetests.xml
 #nosetests -x --verbosity=3 --with-xunit --xunit-file=nosetests.xml --with-coverage
 #nosetests -x --verbosity=3 --with-xunit --xunit-file=nosetests.xml --with-coverage --cover-package=tests
@@ -11,31 +13,30 @@ import unittest
 from code import Math
 
 
+@classmethod
 class MyTest(unittest.TestCase):
-
-
+	"""bla"""
+	
 	def setUp(self):
 		self.session = 'foobar'
-
+	
 	def tearDown(self):
 		self.session = None
-
-	def test_foo(self):	
-		"""test_foo"""			
-		a = 1
-		self.assertEqual(a, 1)
-
+	
+	def test_foo(self):
+		"""test_foo"""
+		var = 1
+		self.assertEqual(var, 1)
+	
 	def test_bar(self):
 		"""test_bar"""		
-		b = 'data' + self.session
-		self.assertEqual(b, 'datafoobar')
-
-	def test_sum(self):  
+		var = 'data' + self.session
+		self.assertEqual(var, 'datafoobar')
+	
+	def test_sum(self):
 		"""Check is sum method is equivalent to operator"""
-		x = Math.sum(self, 1,1)
-		self.assertEqual(x, 2)
-
-
+		var = Math.sum(1,1)
+		self.assertEqual(var, 2)
 
 if __name__ == "__main__":
 	unittest.main()
